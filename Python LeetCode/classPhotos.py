71f9f1c9d84d6924bac9f427b1e3fda5fd142455
Brandon Matran <brandon.matran@galvanize.com>
2023-04-26 15:57:21 -0700

    Updaate

---
+++ Python LeetCode/classPhotos.py
@@ -0,0 +1,20 @@
+def classPhotos(redShirtHeights, blueShirtHeights):
+    # Write your code here.
+    if sum(redShirtHeights) > sum(blueShirtHeights):
+        for i in range(len(redShirtHeights)):
+            if blueShirtHeights[i] <= redShirtHeights[i]:
+                continue
+            return False
+    else:
+        for i in range(len(redShirtHeights)):
+            if blueShirtHeights[i] > redShirtHeights[i]:
+                continue
+            else:
+                return False
+    return True
+
+
+blueShirtHeights = [2, 4, 7, 5, 1]
+redShirtHeights =  [3, 5, 6, 8, 2]
+
+print(classPhotos(redShirtHeights, blueShirtHeights))
---
+++ Python LeetCode/evaluate_expression_tree.py
@@ -0,0 +1,39 @@
+# This is an input class. Do not edit.
+class BinaryTree:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+
+def evaluateExpressionTree(tree):
+    if tree.value >= 0:
+        return tree.value
+    left_value = evaluateExpressionTree(tree.left)
+    print(left_value, "LEFT")
+    right_value = evaluateExpressionTree(tree.right)
+    print(right_value, "RIGHT")
+
+    if tree.value == -1:
+        return left_value + right_value
+    if tree.value == -2:
+        return left_value - right_value
+    if tree.value == -3:
+        return int(left_value / right_value)
+    if tree.value == -4:
+        return left_value * right_value
+
+
+# Create the expression tree from the input JSON
+# Note: In Python, 'null' in JSON is represented as 'None'
+tree1 = BinaryTree(-1,
+                  BinaryTree(2),
+                  BinaryTree(-2,
+                             BinaryTree(4),
+                             BinaryTree(5)
+                             )
+                  )
+
+# Evaluate the expression tree and print the result
+result = evaluateExpressionTree(tree1)
+print("Result:", result)
